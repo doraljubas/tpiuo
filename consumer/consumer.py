735b237ab9c3b89cdb177279a34921296badd53e
Dora Ljubas <dl52294@fer.hr>
2023-11-04 12:39:06 +0100

    lab1

---
+++ consumer/consumer.py
@@ -0,0 +1,16 @@
+from azure.eventhub import EventHubConsumerClient
+
+CONN_STR = "Endpoint=sb://fv-lab1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YQDCiSnN9B6XM+qWTrIWPlUvVW4/tySt1+AEhKUqyWk="
+EVENTHUB_NAME = "reddit-hub"
+CONSUMER_GROUP = "$Default"
+
+client = EventHubConsumerClient.from_connection_string(CONN_STR, CONSUMER_GROUP, eventhub_name=EVENTHUB_NAME)
+
+
+def on_event(partition_context, event):
+    print(event.body_as_str(encoding='UTF-8'))
+    partition_context.update_checkpoint(event)
+
+
+with client:
+    client.receive(on_event=on_event)
---
+++ producer/producer.py
@@ -0,0 +1,34 @@
+import json
+
+import azure.eventhub as eh
+import requests
+from azure.eventhub import EventData
+
+CONN_STR = "Endpoint=sb://fv-lab1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YQDCiSnN9B6XM+qWTrIWPlUvVW4/tySt1+AEhKUqyWk="
+EVENTHUB_NAME = "reddit-hub"
+
+
+def fetch_top_dataengineering_posts():
+    url = "https://www.reddit.com/r/dataengineering/top.json"
+    params = {
+        "limit": "all",
+        "t": 10
+    }
+    response = requests.get(url, params=params, headers={"User-agent": "fer-lab1"})
+    # print(response.json())
+    return response.json().get("data").get("children")
+
+
+def send_posts_to_event_hub(posts):
+    producer = eh.EventHubProducerClient.from_connection_string(conn_str=CONN_STR, eventhub_name=EVENTHUB_NAME)
+    event_data_batch = producer.create_batch()
+    for post in posts:
+        # print(post.get("data"))
+        body = json.dumps(post.get("data")).encode('utf-8')
+        event_data_batch.add(event_data=EventData(body=body))
+    producer.send_batch(event_data_batch)
+
+
+print("Sending data to event hub")
+top_posts = fetch_top_dataengineering_posts()
+send_posts_to_event_hub(top_posts)
