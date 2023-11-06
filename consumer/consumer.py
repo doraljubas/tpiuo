82933038cffa5b233c2926d2c2ca8c45f10e4592
Dora Ljubas <dl52294@fer.hr>
2023-11-06 22:30:26 +0100

    producer & consumer change for task2 in lab2

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,17 +1,50 @@
+import json
+import random
+from datetime import datetime
 from azure.eventhub import EventHubConsumerClient
+from azure.storage.filedatalake import DataLakeServiceClient
+
 
 CONN_STR = "Endpoint=sb://fv-lab1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YQDCiSnN9B6XM+qWTrIWPlUvVW4/tySt1+AEhKUqyWk="
 EVENTHUB_NAME = "reddit-hub"
 CONSUMER_GROUP = "$Default"
 
-client = EventHubConsumerClient.from_connection_string(CONN_STR, CONSUMER_GROUP, eventhub_name=EVENTHUB_NAME)
+STORAGE_ACCOUNT_URL = "https://safervjestina.dfs.core.windows.net"
+STORAGE_ACCOUNT_KEY = "AfQ+3+VclUt8+53NtKlDh+Aitkesd6jpIBquzVNDzkcONo7CpDQKRc0N2ACz7klsB3EKRM30qYJy+AStuMHIqg=="
+CONTAINER_NAME = "lab2"
+
+event_hub_client = EventHubConsumerClient.from_connection_string(CONN_STR, CONSUMER_GROUP, eventhub_name=EVENTHUB_NAME)
+
+
+def get_service_client_account_key() -> DataLakeServiceClient:
+    service_client = DataLakeServiceClient(STORAGE_ACCOUNT_URL, credential=STORAGE_ACCOUNT_KEY)
+    return service_client
 
 
 def on_event(partition_context, event):
-    print(event.body_as_str(encoding='UTF-8'))
+    message_body = json.loads(event.body_as_str(encoding='UTF-8'))
+
+    created_timestamp = message_body.get("created")
+    created_datetime = datetime.fromtimestamp(created_timestamp)
+    directory_path = created_datetime.strftime("%Y/%m/%d/%H/%M")
+
+    adls_client = get_service_client_account_key()
+
+    file_system_client = adls_client.get_file_system_client(CONTAINER_NAME)
+    dir_client = file_system_client.get_directory_client(directory_path)
+    dir_client.create_directory()
+
+    author = message_body.get("author_fullname")
+    file_name=author +"_"+ str(random.randint(100000, 999999)) + ".json"
+    file_client = dir_client.create_file(file_name)
+    file_contents = json.dumps(message_body)
+    file_client.append_data(data=file_contents, offset=0, length=len(file_contents))
+    file_client.flush_data(len(file_contents))
+
+    print(file_name)
     partition_context.update_checkpoint(event)
 
 
-with client:
-    client.receive(on_event=on_event)
-print("DONE")
+with event_hub_client:
+    event_hub_client.receive(on_event=on_event)
+
--- producer/producer.py
+++ producer/producer.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 
 import azure.eventhub as eh
 import requests
@@ -8,29 +9,31 @@ CONN_STR = "Endpoint=sb://fv-lab1.servicebus.windows.net/;SharedAccessKeyName=Ro
 EVENTHUB_NAME = "reddit-hub"
 
 
-def fetch_top_dataengineering_posts():
+def fetch_top_dataengineering_posts(after):
     url = "https://www.reddit.com/r/dataengineering/top.json"
     params = {
         "limit": "all",
-        "t": 10
+        "t": 10,
+        "after": after
     }
     response = requests.get(url, params=params, headers={"User-agent": "fer-lab1"})
-    # print(response.json())
-    return response.json().get("data").get("children")
+    return response.json().get("data", {}).get("children", [])
 
 
 def send_posts_to_event_hub(posts):
     producer = eh.EventHubProducerClient.from_connection_string(conn_str=CONN_STR, eventhub_name=EVENTHUB_NAME)
     event_data_batch = producer.create_batch()
     for post in posts:
-        # print(post.get("data"))
         body = json.dumps(post.get("data")).encode('utf-8')
         event_data_batch.add(event_data=EventData(body=body))
     producer.send_batch(event_data_batch)
 
 
-print("Action works")
-print("Start sending data to event hub!!!")
-top_posts = fetch_top_dataengineering_posts()
-send_posts_to_event_hub(top_posts)
-print("End sending data to event hub")
+after = 0
+while True:
+    posts = fetch_top_dataengineering_posts(after)
+    if not posts:
+        break
+    send_posts_to_event_hub(posts)
+    after += 10
+    sleep(10)
